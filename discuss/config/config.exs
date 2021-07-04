32dd0cc24ba1fe3194fbd979546214679101783e
Bruno Menezes <brunodmenezes@gmail.com>
2021-07-05 10:05:50 +1200

    Added ueberauth to the project and created a dev.secret.exs for sensitive configs. Added configuration for github-oauth.

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "PkOT0eX72kNF1hT6u/5QjsMTAVDg5akvr3k1ICPHGMv5s4M2SAG7/yyd0n64zCkK",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +23,6 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+import_config "dev.secret.exs"
