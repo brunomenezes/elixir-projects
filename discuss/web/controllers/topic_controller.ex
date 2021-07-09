0e2524e7f1edfff810891c7a185a4197b25547a3
Bruno Menezes <brunodmenezes@gmail.com>
2021-07-10 09:18:44 +1200

    Adding security to certain controller actions from plug-module. Added plug-function to check topic-owner on edit/delete/update.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,8 +2,17 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
+  plug :check_topic_owner when action in [:update, :edit, :delete]
+
   def index(conn, _params) do
     render(conn, "index.html", topics: Repo.all(Topic))
+  end
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render(conn, "show.html", topic: topic)
   end
 
   def new(conn, _params) do
@@ -12,8 +21,12 @@ defmodule Discuss.TopicController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+  def create( conn, %{"topic" => topic}) do
+    # changeset = Topic.changeset(%Topic{}, topic)
+
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -55,4 +68,18 @@ defmodule Discuss.TopicController do
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
   end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
+  end
+
 end
