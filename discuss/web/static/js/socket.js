1bc365131bf0f4d10fc99d14813a94fc7178b1e5
Bruno Menezes <brunodmenezes@gmail.com>
2021-07-18 22:10:05 +1200

    Adding websockets support for adding and listing the comments in a specific topic. Code changes to the socket.js file to deal with creating the html elements.

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -4,14 +4,50 @@ let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 socket.connect();
 
-let channel = socket.channel("comments:1", {});
-channel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {});
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      renderComments(resp.comments);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    });
+
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector("button").addEventListener("click", () => {
+    const content = document.querySelector("textarea").value;
+    channel.push("comment:add", { content });
   });
 
-export default socket;
+  return channel;
+};
+
+function renderComment({ comment } = {}) {
+  const renderedComment = commentTemplate(comment);
+  document.querySelector(".collection").innerHTML += renderedComment;
+}
+
+/**
+ * Create a bunch of <li> and look for the <ul> with class .collection and
+ * append the comments
+ * @param {*} comments
+ */
+function renderComments(comments = []) {
+  const renderedComments = comments.map((comment) => {
+    return commentTemplate(comment);
+  });
+  document.querySelector(".collection").innerHTML = renderedComments.join("");
+}
+
+function commentTemplate(comment) {
+  return `
+  <li class="collection-item">
+    ${comment.content}
+  </li>
+`;
+}
+
+window.createSocket = createSocket;
